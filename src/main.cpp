1a81cf9dc7d04feaa8fda8a5f09715e7eafa80fa
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-06-05 14:21:36 +0200

    style: fix formatting

--- src/main.cpp
+++ src/main.cpp
@@ -17,22 +17,24 @@ const int ENCODER_RIGHT_DT = 8;
 const int ENCODER_RIGHT_SW = 7;
 
 const uint16_t colors[] = {
-	Adafruit_NeoMatrix::Color(255,0,0),
-	Adafruit_NeoMatrix::Color(255,255,0),
-	Adafruit_NeoMatrix::Color(0,255,0),
-	Adafruit_NeoMatrix::Color(0,255,255),
-	Adafruit_NeoMatrix::Color(0,0,255),
-	Adafruit_NeoMatrix::Color(255,0,255),
+	Adafruit_NeoMatrix::Color(255, 0, 0),
+	Adafruit_NeoMatrix::Color(255, 255, 0),
+	Adafruit_NeoMatrix::Color(0, 255, 0),
+	Adafruit_NeoMatrix::Color(0, 255, 255),
+	Adafruit_NeoMatrix::Color(0, 0, 255),
+	Adafruit_NeoMatrix::Color(255, 0, 255),
 };
 const size_t NUM_COLORS = 6;
 
 BasicEncoder encoderLeft(ENCODER_LEFT_DT, ENCODER_LEFT_CLK);
 BasicEncoder encoderRight(ENCODER_RIGHT_DT, ENCODER_RIGHT_CLK);
 
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(WIDTH, HEIGHT, PIN_MATRIX,
-															  NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT +
-																	NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
-															  NEO_GRB + NEO_KHZ800);
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(
+	WIDTH,
+	HEIGHT,
+	PIN_MATRIX,
+	NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+	NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
@@ -69,7 +71,8 @@ void loop()
 
 	bool pointChanged = false;
 
-	if (encoderLeft.get_change() || encoderRight.get_change()) {
+	if (encoderLeft.get_change() || encoderRight.get_change())
+	{
 		pointChanged = true;
 		Serial.print("X: ");
 		Serial.print(x);
@@ -79,24 +82,28 @@ void loop()
 
 	bool colorChange = false;
 
-	if (!digitalRead(ENCODER_LEFT_SW)) {
+	if (!digitalRead(ENCODER_LEFT_SW))
+	{
 		currentColor--;
 		colorChange = true;
 	}
 
-	if (!digitalRead(ENCODER_RIGHT_SW)) {
+	if (!digitalRead(ENCODER_RIGHT_SW))
+	{
 		currentColor++;
 		colorChange = true;
 	}
 
-	if (colorChange) {
+	if (colorChange)
+	{
 		currentColor %= NUM_COLORS;
 		Serial.print("Color: ");
 		Serial.println(currentColor);
 		delay(100);
 	}
 
-	if (colorChange || pointChanged) {
+	if (colorChange || pointChanged)
+	{
 		matrix.writePixel(x, y, colors[currentColor]);
 		matrix.show();
 	}
